a19a473d049ab02d85fd8f9773f5880a9e8664d8
Jamie Prince <jp@flareftso.com>
2022-10-19 11:25:20 +0100

    change lists.ts to use external list

--- uniswap-interface-moonbeam/src/constants/lists.ts
+++ uniswap-interface-moonbeam/src/constants/lists.ts
@@ -1,6 +1,5 @@
 // the DCTDEX Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL =
-  '//tokens.json'
+export const DEFAULT_TOKEN_LIST_URL = 'https://viri.uk/i/dex/tokens.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL
